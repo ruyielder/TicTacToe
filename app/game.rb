9ed6db2b0b7141c5910e37d83d3413c57281e8b4
ruyielder <ruyielder@gmail.com>
2014-11-05 22:18:46 +0100

    Updated Game & launcher

--- app/game.rb
+++ app/game.rb
@@ -1,7 +1,17 @@
+require_relative 'board'
+
 class Game
 
+  def initialize(board, clients)
+    @board = board
+    @clients = clients
+  end
+
   def run
-    
+    @clients.cycle do |client|
+      index = client.choice_field(@board)
+      @board.take_field(index, client.field_value)
+    end
   end
 
 end
--- launcher.rb
+++ launcher.rb
@@ -1,3 +1,11 @@
 require_relative 'app/game'
+require_relative 'app/board'
+require_relative 'app/player_client'
 
-Game.new.run
+board = Board.create_with_default_size
+clients = [
+    PlayerClient.new(:'X'),
+    PlayerClient.new(:'O')
+]
+game = Game.new(board, clients)
+game.run
