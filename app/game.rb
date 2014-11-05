594a53413cee2923ae4bd912bd070cd05f8770a3
ruyielder <ruyielder@gmail.com>
2014-11-05 23:48:35 +0100

    Updated Game class

--- app/game.rb
+++ app/game.rb
@@ -8,9 +8,27 @@ class Game
   end
 
   def run
+    winner_field_value = get_winner_field_value
+    display_result(winner_field_value)
+  end
+
+  def get_winner_field_value
     @clients.cycle do |client|
       index = client.choice_field(@board)
       @board.take_field(index, client.field_value)
+      if @board.contains_filled_line_by? client.field_value
+        return client.field_value
+      elsif @board.is_full_filled?
+        return nil
+      end
+    end
+  end
+
+  def display_result(winner_field_value)
+    if winner_field_value.nil?
+      puts 'REMIS'
+    else
+      puts "WYGRAŁ GRACZ #{winner_field_value}, GRATULACJE!!!"
     end
   end
 
