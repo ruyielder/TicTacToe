07e5cac9fc26f3af729bbc3ccb7205ea00f06ec9
ruyielder <ruyielder@gmail.com>
2014-11-05 22:18:22 +0100

    Created PlayerClient

--- app/player_client.rb
+++ app/player_client.rb
@@ -5,25 +5,41 @@ require_relative 'index_validator'
 class PlayerClient < Client
 
   def choice_field(board)
+    puts board.render
+    get_index_from_user(board)
+  end
+
+  def get_index_from_user(board)
     digits_validator = DigitsValidator.new
     index_validator = IndexValidator.new(board)
-    print 'Podaj numer pola jaki chcesz zająć: '
+    print_message 'Podaj numer pola jaki chcesz zająć'
 
     loop do
-      user_value = gets
+      user_value = get_value
 
       unless digits_validator.is_valid? user_value
-        puts 'Podana wartość nie jest liczbą, spróbuj jeszcze raz: '
+        print_message 'Podana wartość nie jest liczbą! Spróbuj jeszcze raz'
+        next
       end
 
       user_index = user_value.to_i
 
       unless index_validator.is_valid?(user_index)
-        puts 'Podany index jest już zajęty, spróbuj jeszcze raz: '
+        print_message 'Podany index jest już zajęty! Spróbuj jeszcze raz'
+        next
       end
 
       return user_index
     end
+  end
+
+  def print_message(message)
+    puts "[#{message}]"
+  end
+
+  def get_value
+    print "GRACZ #{@field_value}> "
+    gets
   end
 
 end
